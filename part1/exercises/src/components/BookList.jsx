8f96452499bfc10f600ff041409ad8f89dfa1593
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-03-08 22:09:44 -0600

    Class 18 - React 1 Exercises initial save

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Better Section Heading";
+   let book1 = "https://sp.yimg.com/ib/th?id=OPHS.yNAHfOQVY5ml4g474C474&o=5&pid=21.1&w=174&h=174";
+   let book2 = "https://sp.yimg.com/ib/th?id=OPHS.cKy7VrA%2bHEeFiA474C474&o=5&pid=21.1&w=174&h=174";
+   let book3 = "https://sp.yimg.com/ib/th?id=OPHS.tM0hw5WV5Os1Dg474C474&o=5&pid=21.1&w=174&h=174";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Identity by Nora Roberts" />
+         <img src={book2} alt="Burried Secrets by T.J. Brearton" />
+         <img src={book3} alt="Against All Odds by Alex Kershaw" />
       </div>      
    );
 }
