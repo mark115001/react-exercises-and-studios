2e5907e13d41e3a2a8483892d2d1b8f9b0c4be2a
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-03-13 22:35:07 -0500

    Class 19 - React Exercises 2

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -1,9 +1,17 @@
 import './styles.css';
 
+
+
 function Button() {
+   function onLearnMore() {
+      return(
+         alert("Splish Splash I was taking a bath")
+      )
+   }
 
    return ( 
-         <button>
+         // <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
